d8f5abfb0a5fd604f94183ef05eb58aa7a4a85ed
Rohit Selote <workforrohitselote@gmail.com>
2026-01-01 15:15:45 +0530

    Add files via upload

---
+++ java_beginner/ScannerExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class ScannerExample {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter Name.");
+        String name = S.nextLine();
+        System.out.println("Enter age");
+        int age =S.nextInt();
+        System.out.println("Name:" +name);
+        System.out.println("Age: "+age);
+        
+    }
+
+    
+}
+
+
+    
+
